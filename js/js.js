98dee648201943ed9a23cff76931cdfe9d8953bb
annakaup <anna.kaup@tptlive.ee>
2023-06-08 11:45:11 +0300

    alligment added

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-/* $(document).ready(function(){
+$(document).ready(function(){
     
     $(".owl-carousel").owlCarousel({
         items: 5,
@@ -28,7 +28,7 @@
 
     });
     
-  });  */
+  });  
 
-  $('.owl-carousel').owlCarousel({ loop:true, margin:10, responsiveClass:true, responsive:{ 0:{ items:1, nav:true }, 600:{ items:2, nav:false }, 1000:{ items:3, nav:true, loop:false } } })
+//   $('.owl-carousel').owlCarousel({ loop:true, margin:10, responsiveClass:true, responsive:{ 0:{ items:1, nav:true }, 600:{ items:2, nav:false }, 1000:{ items:3, nav:true, loop:false } } })
 
