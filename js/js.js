e07211c594e87a67eb95cec12e59088a1febbf0d
annakaup <anna.kaup@tptlive.ee>
2023-06-08 16:49:36 +0300

    aligment added

--- js/js.js
+++ js/js.js
@@ -4,23 +4,23 @@ $(document).ready(function(){
         items: 5,
         loop: true,
         center: false,
-        autoplay: true,
+        autoplay: false,
         autoPlaySpeed: 6000,
         autoPlayTimeout: 6000,
         autoplayHoverPause: true,
         dots: true,
+        nav: true,
         margin:20,
         
-
         responsive: {
             0:{
-                items:1
+                items:1,
             },
             800: {
                 items:3
             },
             1100:{
-                items:5
+                items:5,
             }
 
         } 
@@ -29,6 +29,7 @@ $(document).ready(function(){
     });
     
   });  
+
 
 //   $('.owl-carousel').owlCarousel({ loop:true, margin:10, responsiveClass:true, responsive:{ 0:{ items:1, nav:true }, 600:{ items:2, nav:false }, 1000:{ items:3, nav:true, loop:false } } })
 
